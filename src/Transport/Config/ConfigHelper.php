de32ec8464049261054c9cbd1ee8dd8114052228
Michal Kral <michal.kral@startupjobs.cz>
2026-03-17 10:51:00 +0100

    Remove unnecessary assertion from `ConfigHelper::getArray` method

--- src/Transport/Config/ConfigHelper.php
+++ src/Transport/Config/ConfigHelper.php
@@ -65,9 +65,6 @@ class ConfigHelper
         /** @var array<string, mixed>|null $filteredValue */
         $filteredValue = self::getType($config, $key, self::TYPE_ARRAY);
 
-        // @phpstan-ignore-next-line
-        assert(is_array($filteredValue) || $filteredValue === null);
-
         return $filteredValue;
     }
 
