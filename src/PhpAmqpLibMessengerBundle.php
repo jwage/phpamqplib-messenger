f735407755b1137607eadeef2fa21001f4ee9b34
Jonathan H. Wage <jwage@traderspost.io>
2026-06-18 14:06:32 -0400

    Fix coding standards.

--- src/PhpAmqpLibMessengerBundle.php
+++ src/PhpAmqpLibMessengerBundle.php
@@ -9,9 +9,7 @@ use Override;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * @psalm-suppress DeprecatedInterface
- */
+/** @psalm-suppress DeprecatedInterface */
 class PhpAmqpLibMessengerBundle extends Bundle
 {
     #[Override]
